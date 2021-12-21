8c7c43fb9efe552fb47eb960617288d0769733ae
arunvinu <arunkumarxworkz@gmail.com>
2021-12-21 17:56:44 +0545

    program3

---
+++ Students.java
@@ -0,0 +1,7 @@
+class Students{
+public static void main (String [ ] a){
+float [ ] percentage = {75.5f, 65.15f, 69.16f, 95.6f, 85.5f, 56.5f,};
+System.out.println(percentage [ 0 ]);
+System.out.println(percentage [ 1 ]);
+}
+}
